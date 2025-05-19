f1df7718ed50e94ecc507412f8c99dc347d0685f
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-05-19 16:34:16 -0600

    current page highlight done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from './nav.module.css'
 
 export default function Nav() {
+    const location = useLocation();
     return (
         <div className={styles.divNavigation}>
             <Link to="/"><img className={styles.img}src="src\assets\NASA Rovor logo.webp" alt="NASA Rover logo" /></Link>
-            <Link to="/">Home</Link>
-            <Link to="/potd">Picture of the Day</Link>
-            <Link to="/search">Search</Link>            
-            <Link to="/favorites">Favorites</Link>            
-            <Link to="/acknowledgements">Acknowledgements</Link>            
+            <Link to="/" className={location.pathname === "/" ? styles.active : ""}>Home</Link>
+            <Link to="/potd" className={location.pathname === "/potd" ? styles.active : ""}>Picture of the Day</Link>
+            <Link to="/search" className={location.pathname === "/search" ? styles.active : ""}>Search</Link>            
+            <Link to="/favorites" className={location.pathname === "/favorites" ? styles.active : ""}>Favorites</Link>            
+            <Link to="/acknowledgements" className={location.pathname === "/acknowledgements" ? styles.active : ""}>Acknowledgements</Link>            
         </div>
     );
 }
