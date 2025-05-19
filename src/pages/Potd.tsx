6213d39c3a489ce0c60ae3d3fd85308b570c51af
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-05-19 15:30:42 -0600

    Added useEffect to the picture of the Day

--- src/pages/Potd.tsx
+++ src/pages/Potd.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react"
+
+
 export default function Potd() {
+  const apiKey = import.meta.env.VITE_NASA_API_KEY;
+  const [picture,setPicture] = useState('');
+
+  const potd = useEffect(() => {
+    const interval = setTimeout(() => {
+      setPicture('123');
+    }, 1000);
+    console.log("Running...");
+
+    return () => clearInterval(interval);
+  })
+
   return (<>
-    <h1>This is the Picture of the Day</h1>
-    <img src="src\assets\NASA Under Construction 404.png" alt="Picture of aliens trying to fix Mars Rover (404 not found)" style={{ width: '90%' }} />
+    <h1>Picture of the Day</h1>
+
+    {(picture)? <img src="src\assets\NASA Under Construction 404.png" alt="Picture of alien with the magnifing glass and title searching" style={{ width: '90%' }} />:<img src="src\assets\NASA Alien searching.png" alt="Picture of alien with the magnifing glass and title searching" style={{ width: '90%' }} />}
   </>)
 }
