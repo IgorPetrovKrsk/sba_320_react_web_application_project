67da4c39e28a0df82a987a7b6dd28a01f4e34318
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-05-19 16:22:09 -0600

    Getting new picture on the day done

--- src/pages/Potd.tsx
+++ src/pages/Potd.tsx
@@ -1,22 +1,46 @@
 import { useEffect, useState } from "react"
+import axios from "axios";
 
 
 export default function Potd() {
-  const apiKey = import.meta.env.VITE_NASA_API_KEY;
-  const [picture,setPicture] = useState('');
+  const apiKey = import.meta.env.VITE_NASA_API_KEY ?? 'DEMO_KEY'; //api key or DEMO_KEY is no API key found in envirenment
+  const [responceData, setResponceData] = useState<{ [key: string]: any }>({}); //objects with any type
+  const [loading, setLoading] = useState(true)
 
-  const potd = useEffect(() => {
-    const interval = setTimeout(() => {
-      setPicture('123');
+  useEffect(() => {
+    const interval = setTimeout(async () => {
+      const responce = await axios.get('https://api.nasa.gov/planetary/apod', {
+        params: {
+          api_key: apiKey,
+          count: 1,
+          thumbs: true
+        }
+      });
+      if (responce.status == 200) {        
+        setResponceData(responce.data[0]);
+      }
+      setLoading(false);
     }, 1000);
-    console.log("Running...");
 
-    return () => clearInterval(interval);
-  })
+    return () => clearTimeout(interval);
+  },[loading])
 
   return (<>
     <h1>Picture of the Day</h1>
 
-    {(picture)? <img src="src\assets\NASA Under Construction 404.png" alt="Picture of alien with the magnifing glass and title searching" style={{ width: '90%' }} />:<img src="src\assets\NASA Alien searching.png" alt="Picture of alien with the magnifing glass and title searching" style={{ width: '90%' }} />}
+    {(loading) ?
+      <img src="src\assets\NASA Alien searching.png" alt="Picture of alien with the magnifing glass and title searching" style={{ width: '90%' }} /> :
+      <>
+        <h3>Date {responceData.date}</h3>
+        <h3>{responceData.title}</h3>
+        <p>{responceData.explanation}</p>
+        {(responceData.media_type=='image')?
+          <img src={responceData.url} alt={responceData.title} style={{ width: '90%' }} /> :
+          <></>
+        }        
+      </>
+    }
+    <br />
+    <button disabled={loading} onClick={()=> {setLoading(true)}}>Get new picture of the day</button>
   </>)
 }
